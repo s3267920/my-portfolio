a99df7857f5bdadea208903c420f72f4795c60bd
lucychen <s3267920@gmail.com>
2023-07-05 20:38:16 +0800

    D:Add Skill item component

---
+++ src/components/skills/SkillDetail.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { SkillInfoProps } from '@/views/SkillsView.vue';
+
+defineProps<{
+	data?: SkillInfoProps
+}>()
+</script>
+<template>
+	<div class="p-2">
+		<h4 class=" text-2xl font-semibold text-cyan-800">{{ data?.title }}</h4>
+		<ul class="pl-4" v-if="data">
+			<li class=" list-disc" v-for="item in data.detail" :key="item.title">{{ item.title }}</li>
+		</ul>
+	</div>
+</template>
---
+++ src/components/skills/SkillItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { SkillInfoProps } from '@/views/SkillsView.vue';
+import SkillDetail from '@/components/skills/SkillDetail.vue'
+defineProps<{
+	data?: SkillInfoProps
+}>()
+</script>
+<template>
+	<div class="p-2">
+		<h3 class=" text-3xl font-semibold text-neutral-600 capitalize">{{ data?.title }}</h3>
+		<div class="p-2" v-if="data">
+			<div v-for="item in data.detail" :key="item.title">
+				<SkillDetail :data="item" />
+			</div>
+		</div>
+
+	</div>
+</template>
