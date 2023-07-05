b112016bf79804f519d69665b513c612acc8f221
lucychen <s3267920@gmail.com>
2023-07-05 15:38:31 +0800

    D:Remove unused file

--- src/views/PortfolioView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div className='grid grid-rows-5 row-start-2 min-h-screen bg-white shadow-md rounded-lg'>
-    <article className=' p-2 m-5 shadow-md'></article>
-    <article className=' p-2 m-5 shadow-md'></article>
-  </div>
-</template>
