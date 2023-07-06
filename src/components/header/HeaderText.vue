ee6be00e1e7c4564dea61a15a18a82f8d5946d4b
lucychen <s3267920@gmail.com>
2023-07-06 18:07:37 +0800

    D:Adjust style to prevent flutter

--- src/components/header/HeaderText.vue
+++ src/components/header/HeaderText.vue
@@ -5,9 +5,9 @@ const { t } = useI18n({ messages: message });
 </script>
 <template>
 	<div
-		class="order-3 flex flex-col justify-center col-span-full mt-5   bg-cyan-50 bg-opacity-40 p-3 rounded-md cursor-pointer transition-colors  duration-300 hover:bg-opacity-100  lg:col-start-5 lg:col-end-18 lg:row-span-2 lg:col-span-5 lg:order-2">
-		<p class="text-2xl items-center flex mb-1" v-if='t("slogan")'><b>{{ t("slogan") }}</b></p>
-		<p class="about-me text-lg items-center flex " v-html="t('aboutMe')"></p>
+		class="order-3 flex flex-col justify-center col-span-full mt-5   bg-cyan-50 bg-opacity-40 p-3 rounded-md cursor-pointer transition-colors  duration-300 hover:bg-opacity-100 lg:mt-0 lg:min-h-[230px] lg:col-start-5 lg:col-end-18 lg:row-span-2 lg:col-span-5 lg:order-2">
+		<p class="text-2xl items-center flex mb-auto" v-if='t("slogan")'><b>{{ t("slogan") }}</b></p>
+		<p class="about-me text-lg items-center flex mb-auto" v-html="t('aboutMe')"></p>
 	</div>
 </template>
 <style lang="scss">
