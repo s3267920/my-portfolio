da83c69c7c7481e53596b7ae08bd13a34aaace95
lucychen <s3267920@gmail.com>
2023-07-06 17:39:32 +0800

    D:Add Vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import './assets/scss/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(
+  createI18n({
+    allowComposition: true,
+    legacy: false,
+    locale: 'zh-TW',
+    fallbackLocale: 'zh-TW'
+  })
+)
 app.use(router)
 
 app.mount('#app')
