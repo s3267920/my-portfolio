53a76eeebca194696564c533438e8811fddb3aa7
lucychen <s3267920@gmail.com>
2023-07-07 09:27:09 +0800

    D:Add common section title component

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class=" text-4xl text-cyan-950 font-bold">
+		<slot></slot>
+		<div class=" border-b-4 mt-3"></div>
+	</div>
+</template>
+
+
+
+<style lang="scss" scoped></style>
