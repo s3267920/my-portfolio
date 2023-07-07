fd4e7d8faabb67b91c12741052183bcb02c8af24
lucychen <s3267920@gmail.com>
2023-07-07 08:17:05 +0800

    D:Add contact email

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,9 +5,10 @@ import HeaderText from '@/components/header/HeaderText.vue'
 <template>
 	<header class='header grid-rows-1 grid grid-cols-9 mt-8 ml-2 mb-8 w-full'>
 		<h1
-			class='header-logo title p-5 text-7xl bg-white rounded-lg shadow-md relative flex flex-col col-start-1 col-span-11 md:max-w-[400px]  md:col-span-4 md:min-w-[330px] order-1'>
-			<span class="font-semibold text-cyan-950">陳柔妡</span>
-			<span class="text-4xl text-right text-cyan-800">Lucy Chen</span>
+			class='header-logo title p-5 pb-2  bg-white rounded-lg shadow-md relative flex flex-col col-start-1 col-span-11 md:max-w-[400px]  md:col-span-4 md:min-w-[330px] order-1 '>
+			<span class="text-6xl font-semibold text-cyan-950">陳柔妡</span>
+			<span class="text-4xl font-semibold text-right text-cyan-800">Lucy Chen</span>
+			<span class="text-xl text-left font-semibold text-neutral-700 mt-auto">s3267920@gmail.com</span>
 		</h1>
 		<Suspense>
 			<HeaderText class="header-text" />
