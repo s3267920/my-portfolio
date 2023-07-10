1faa7fac85fc4458dd5e967553c7a490e39df37f
lucychen <s3267920@gmail.com>
2023-07-10 19:16:22 +0800

    D:Remove about router

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,11 +10,6 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/skills',
       name: 'skills',
       component: () => import('../views/SkillsView.vue')
