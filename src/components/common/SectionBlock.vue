cbba4587c8b0b0f166ea548e2c6b532a52e4c91e
lucychen <s3267920@gmail.com>
2023-07-10 09:09:13 +0800

    D:Add common Section component

---
+++ src/components/common/SectionBlock.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+	isSection?: boolean,
+}>()
+</script>
+<template>
+	<section class='p-2 m-5 shadow-md bg-white  flex flex-col print:m-2 print:shadow-none '
+		:class="{ full: !props.isSection, 'p-5': !props.isSection }">
+		<slot></slot>
+	</section>
+</template>
