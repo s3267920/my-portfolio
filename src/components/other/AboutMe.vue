0fc7e5ef045d323c085b8ddd3f9a5d06f1ec9331
lucychen <s3267920@gmail.com>
2023-07-10 18:35:31 +0800

    D:Add about me section

---
+++ src/components/other/AboutMe.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import message from '@/locales/aboutMe.json'
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n({ messages: message });
+</script>
+<template>
+	<div
+		class="flex-col">
+		<p class="text-lg items-center flex mb-auto whitespace-pre-wrap" v-html="t('aboutMe')"></p>
+
+	</div>
+</template>
