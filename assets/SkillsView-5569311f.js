2c72cb0412b30b937d9722c5535bb37fb1503b98
lucychen <s3267920@gmail.com>
2023-07-11 08:13:50 +0800

    D:Adjust path deploy

---
+++ assets/SkillsView-5569311f.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./index-07bfdad1.js";import"./vendor-730932f9.js";export{m as default};
--- assets/SkillsView-6c8da07c.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./index-9075f1c5.js";import"./vendor-730932f9.js";export{m as default};
