d20dedf4d6fe9e5d1a5593c04486f122ec965c1c
lucychen <s3267920@gmail.com>
2023-07-11 08:16:49 +0800

    D:Adjust path deploy

---
+++ assets/OtherView-51dbc09b.js
@@ -0,0 +1,1 @@
+import{b as m}from"./index-dc6d1551.js";import"./vendor-730932f9.js";export{m as default};
--- assets/OtherView-93a5591b.js
+++
@@ -1,1 +0,0 @@
-import{b as m}from"./index-07bfdad1.js";import"./vendor-730932f9.js";export{m as default};
